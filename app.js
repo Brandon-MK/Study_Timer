b0aeff73621fe5885df1944660f88504e43e8e02
Daniel John <danieljohncode@tutanota.com>
2020-09-19 23:22:04 +0100

    Displaying the data to the user in the form of a clock

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("**STUDY TIMER BY BRANDON_MK & DANIELJCODE**")
 const timerDisplay = document.getElementById("display");
 
 var secondsIn25mins = 1500;
@@ -8,15 +9,32 @@ var secondsLeft = secondsIn25mins;
 
 function Countdown(){
 
+    console.log("------- STUDY TIMER INFO -------")
+
     secondsLeft = (secondsLeft - 1);
-    console.log(`seconds left: ${secondsLeft}`);
+    console.log(`seconds left (In total): ${secondsLeft}`);
 
     var mind = secondsLeft % (60 * 60);
     var minutes = Math.floor(mind / 60);
-    //console.log(minutes);
+    console.log(`Minutes left (In total):  ${minutes}`);
 
     var secd = secondsLeft % 60;
     var seconds = Math.ceil(secd);
-    console.log(seconds);
+    console.log(`seconds left (This cycle): ${seconds}`);
+
+    DisplayDataToUser(seconds, minutes);
 
 }setInterval(Countdown, 1000);
+
+function DisplayDataToUser(secondslft, minuteslft){
+ 
+     
+
+    if(secondslft < 10){
+
+        timerDisplay.innerHTML = `${minuteslft}:0${secondslft}`;//Adding a zero before the seconds if the seconds value is less than 10 (9, 8, 7, etc)
+
+    }else{
+        timerDisplay.innerHTML = `${minuteslft}:${secondslft}`;   
+    }
+}
