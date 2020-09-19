7a66bcdecea86796f275f236de16ee73399ed5af
Daniel John <danieljohncode@tutanota.com>
2020-09-19 23:12:23 +0100

    Got the system calculating seconds and minutes
    Thank fuck for stackoverflow! :laughing:

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const timerDisplay = document.getElementById("display");
 
+var secondsIn25mins = 1500;
+var millisecondsIn25mins = 1500 * 1000
+
+var secondsLeft = secondsIn25mins;
+
+
 function Countdown(){
 
-    console.log("1 second has passed");
+    secondsLeft = (secondsLeft - 1);
+    console.log(`seconds left: ${secondsLeft}`);
 
-}setInterval(Countdown(), 1000);
+    var mind = secondsLeft % (60 * 60);
+    var minutes = Math.floor(mind / 60);
+    //console.log(minutes);
+
+    var secd = secondsLeft % 60;
+    var seconds = Math.ceil(secd);
+    console.log(seconds);
+
+}setInterval(Countdown, 1000);
